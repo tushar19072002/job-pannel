e43a60ed80a36a7a7122e8be672688728af7a8e9
tushar19072002 <tusharmakhija237@gmail.com>
2024-05-31 15:27:19 +0530

    logo updated

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import JobModal from './JobModal';
+import Logo from "../images/logo.jpg"
 
 function Nav() {
   const navigate = useNavigate();
@@ -36,8 +37,8 @@ function Nav() {
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
           <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <img src={Logo} className="h-8" alt="Flowbite Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Thinkscoop Technologies</span>
           </a>
           <div className="flex items-center space-x-6 rtl:space-x-reverse">
             {isLoggedIn ? (
