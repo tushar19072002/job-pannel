e2af2d05ebcaea334b72c1e51da5eceaddba0dfe
tushar19072002 <tusharmakhija237@gmail.com>
2024-06-01 17:53:11 +0530

    location of deactivate button changed

--- src/Components/ViewUpdateJobModal.js
+++ src/Components/ViewUpdateJobModal.js
@@ -494,7 +494,7 @@ const ViewUpdateJobModal = ({ isOpen, onClose, jobDetails}) => {
               type="button"
               onClick={handleDeactivateClick}
               disabled={!isEditMode}
-              className={`font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ${
+              className={`font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 float-right ${
                 !isEditMode
                   ? 'hidden'
                   : 'text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80'
